00e9153679cb5b3a3694a3330fe94580d2f38e13
Zuzana Žillová <Zuzana illov@192.168.100.10>
2021-02-11 09:53:15 +0100

    imitial commit.

---
+++ src/Hra_Opatrenia/Opatrenie.java
@@ -0,0 +1,10 @@
+package Hra_Opatrenia;
+
+/**
+ * @author Zuzana Žillová
+ */
+public interface Opatrenie {
+    public boolean zapnutie();
+    public boolean vypnutie();
+    public double getIndex();
+}
